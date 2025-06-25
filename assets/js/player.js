ab2f85477edcd3232f20d899e03f9be7491ca6e4
Faridaartawan <farida.artawan@gmail.com>
2025-06-25 20:20:04 +0700

    asdsadasdsad

---
+++ assets/js/player.js
@@ -0,0 +1,8 @@
+const player = new Plyr('#player', {
+    controls: ['play', 'progress', 'mute', 'volume', 'fullscreen'],
+    youtube: {
+      rel: 0,
+      showinfo: 0,
+      modestbranding: 1
+    }
+  });
